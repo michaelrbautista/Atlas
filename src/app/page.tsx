f2ddf740843d0110ef1918d611b8a06687e026d2
michael-bautista <mrbautistaios@gmail.com>
2024-07-18 19:47:20 -0700

    added final section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,6 @@ import { getAllPrograms } from "./actions";
 import { Input } from "@/components/ui/input";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
-import webPreview from "../../public/webPreview.png";
-import mobilePreview from "../../public/mobilePreview.png";
 import Logo from "@/components/logo";
 import { onJoin } from "./onJoin";
 import { cn } from "@/lib/utils";
@@ -63,15 +61,15 @@ export default function Home() {
         <div className="fixed w-full h-20 flex px-5 bg-background">
           <Logo></Logo>
         </div>
-        <div className="pt-40 flex flex-col gap-2 w-full justify-center items-center">
-          <div className="flex flex-col gap-2 w-4/5 sm:w-2/4">
-            <p className="text-primaryText text-4xl md:text-6xl font-bold text-center">
+        <div className="py-40 flex flex-col gap-2 justify-center items-center">
+          <div className="flex flex-col items-center gap-2 w-4/5 sm:w-2/4 max-w-3xl">
+            <p className="text-primaryText text-4xl sm:text-6xl font-black text-center">
               You can go D1.
             </p>
-            <p className="text-secondaryText text-lg md:text-4xl font-medium text-center">
+            <p className="text-secondaryText text-lg sm:text-4xl font-medium text-center">
               Get access to training and nutrition programs to help you play at the next level.
             </p>
-            <div className="flex flex-col mx-auto w-full sm:w-3/4 pt-10 gap-5">
+            <div className="flex flex-col items-center justify-center w-full sm:max-w-lg pt-10 gap-5">
               <Input ref={emailRef} type="email" placeholder="Enter your email"></Input>
               {isSigningUp ?
                   <Button disabled>
@@ -92,14 +90,24 @@ export default function Home() {
               </p>
             }
           </div>
-          <div className="flex flex-col md:flex-row py-20 px-10 gap-5 justify-center items-center">
-              <div className="">
-                <Image layout="intrinsic" src="/webPreview.png" width={600} height={512} alt="web"></Image>
+          <div className="flex flex-col md:flex-row py-20 justify-center items-center w-4/5 max-w-3xl">
+            <div className="">
+              <Image layout="intrinsic" src="/landingPageImage.png" width={800} height={512} alt="web"></Image>
+            </div>
+          </div>
+          <div className="bg-systemGray5 flex flex-col gap-5 sm:gap-10 w-4/5 max-w-3xl rounded-xl p-5 sm:p-10">
+            <p className="text-primaryText font-black text-xl sm:text-3xl">Everything you need to unlock your athleticism.</p>
+            <div className="flex flex-col gap-5">
+              <div className="bg-systemGray4 p-3 sm:p-5 rounded-xl flex flex-row gap-3 sm:gap-5 items-center">
+                <p className="min-w-10">🏃‍♂️🏋️‍♂️</p>
+                <p className="text-primaryText font-bold text-sm sm:text-lg">Training, strength, and conditioning programs to improve your performance.</p>
               </div>
-              <div className="">
-                <Image layout="intrinsic" src="/mobilePreview.png" width={180} height={844} alt="web"></Image>
+              <div className="bg-systemGray4 p-3 sm:p-5 rounded-xl flex flex-row gap-3 sm:gap-5 items-center">
+                <p className="min-w-10">🍴💤</p>
+                <p className="text-primaryText font-bold text-sm sm:text-lg">Nutrition and lifestyle protocols to consistently perform and avoid injury.</p>
               </div>
             </div>
+          </div>
         </div>
       </div>
       // <div className="flex flex-col w-full px-16 md:px-60 pt-10">
