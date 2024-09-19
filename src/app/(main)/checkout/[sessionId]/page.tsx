ccdda76b172cd2aac1232ddc6e84f5c896a6f56d
michael-bautista <mrbautistaios@gmail.com>
2024-09-19 16:48:15 -0700

    Finished checkout page

--- src/app/(main)/checkout/[sessionId]/page.tsx
+++ src/app/(main)/checkout/[sessionId]/page.tsx
@@ -2,7 +2,15 @@
 
 const ReturnPage = async () => {
     return (
-        <div className="text-primaryText">Payment submitted</div>
+        <div className="flex flex-col w-full sm:max-w-3xl px-5 py-20 sm:py-10 gap-10 sm:gap-10">
+            <div className="flex flex-col items-center gap-5">
+                <p className="text-primaryText text-3xl font-bold">Payment has been submitted.</p>
+                <p className="text-secondaryText text-base font-semibold text-center">
+                    Once approved, you will be able to view the program in the “Programs” tab or in the iOS app (Atlas: Health and Fitness).
+                    If you have any issues, please reach out to mrbautistadev@gmail.com.
+                </p>
+            </div>
+        </div>
     )
 }
 export default ReturnPage
