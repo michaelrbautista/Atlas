df0c2a776aa83f7d4bb6c3fdd5919a2bb83b4ff0
michael-bautista <mrbautistadev@gmail.com>
2024-12-02 15:09:50 -0800

    added debounce to search

--- src/components/explore/Search.tsx
+++ src/components/explore/Search.tsx
@@ -3,31 +3,27 @@
 import { SearchIcon } from "lucide-react";
 import { Input } from "../ui/input";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = () => {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
-    const [query, setQuery] = useState("");
 
-    const handleSearch = () => {
+    const handleSearch = useDebouncedCallback((term: string) => {
         const params = new URLSearchParams(searchParams);
 
-        if (query) {
-            params.set("query", query);
+        if (term) {
+            params.set("query", term);
         } else {
             params.delete("query");
         }
 
         replace(`${pathname}?${params.toString()}`);
-    }
+    }, 300);
 
     return (
-        <form onSubmit={(e) => {
-            e.preventDefault();
-            handleSearch();
-        }} className="relative w-full">
+        <div className="relative w-full">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <SearchIcon className="w-5 h-5 text-muted-foreground" />
             </div>
@@ -38,10 +34,10 @@ const Search = () => {
                 className="w-full rounded-md bg-muted pl-10 pr-4 py-2 text-foreground focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary"
                 defaultValue={searchParams.get("query")?.toString()}
                 onChange={(e) => {
-                    setQuery(e.target.value);
+                    handleSearch(e.target.value);
                 }}
             />
-        </form>
+        </div>
     )
 }
 export default Search
