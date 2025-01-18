6eb904a6c0fafeec91f0556db8fab69896fdb035
michael-bautista <mrbautistadev@gmail.com>
2025-01-17 17:13:42 -0800

    Added help button

--- src/components/sidebar/UserInfo.tsx
+++ src/components/sidebar/UserInfo.tsx
@@ -3,6 +3,13 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle
+} from "@/components/ui/dialog";
+import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
@@ -13,6 +20,7 @@ import LogoutButton from "../auth/LogoutButton";
 import { Ellipsis } from "lucide-react";
 import { logout } from "@/server-actions/auth";
 import { useToast } from "../ui/use-toast";
+import { useState } from "react";
 
 const UserInfo = ({
     id,
@@ -23,6 +31,8 @@ const UserInfo = ({
     fullName: string,
     username: string
 }) => {
+    const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
     const { toast } = useToast();
 
     async function clientLogout() {
@@ -38,26 +48,44 @@ const UserInfo = ({
     }
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="userInfo">
-                    <div className="flex flex-col p-2 items-start">
-                        <p className="text-foreground text-base font-bold line-clamp-1">{fullName}</p>
-                        <p className="text-muted-foreground text-sm font-normal line-clamp-1">@{username}</p>
-                    </div>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuItem className="p-0" asChild>
-                    <Link href={`/${username}`} className="w-full h-full px-2 py-1.5 text-start cursor-pointer">View profile</Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="p-0" asChild>
-                    <form action={clientLogout}>
-                        <button className="w-full h-full px-2 py-1.5 text-start">Logout</button>
-                    </form>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
+            <DialogContent className="bg-background max-w-96 sm:max-w-md">
+                <DialogHeader>
+                    <DialogTitle>Help</DialogTitle>
+                    <DialogDescription>For help, please send an email to mrbautistadev@gmail.com</DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="userInfo">
+                        <div className="flex flex-col p-2 items-start">
+                            <p className="text-foreground text-base font-bold line-clamp-1">{fullName}</p>
+                            <p className="text-muted-foreground text-sm font-normal line-clamp-1">@{username}</p>
+                        </div>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem asChild>
+                        <Link href={`/${username}`} className="w-full h-full px-2 py-1.5 text-start cursor-pointer">View profile</Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                        <Button
+                            className="justify-start h-8 cursor-pointer"
+                            variant="ghost"
+                            size="full"
+                            onClick={() => {setDialogIsOpen(true)}}
+                        >
+                            Help
+                        </Button>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                        <form action={clientLogout}>
+                            <button className="w-full text-start">Logout</button>
+                        </form>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </Dialog>
     );
 }
  
