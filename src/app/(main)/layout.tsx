c5bed2a67c6893c0b35694cc440d61fc688cb418
michael-bautista <mrbautistadev@gmail.com>
2025-02-04 20:56:55 -0800

    removed next/head

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "../globals.css";
 import MobileSidebar from "@/components/sidebar/mobile/MobileSidebar";
 import Sidebar from "@/components/sidebar/Sidebar";
@@ -11,20 +9,12 @@ export default function UserLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Head>
-          <meta
-              name="apple-itunes-app"
-              content="app-id=6484401731"
-          />
-      </Head>
-      <div className="flex">
-          <Sidebar></Sidebar>
-          <div className="flex flex-col w-full items-start sm:items-center">
-            <MobileSidebar></MobileSidebar>
-            {children}
-          </div>
-      </div>
-    </>
+    <div className="flex">
+        <Sidebar></Sidebar>
+        <div className="flex flex-col w-full items-start sm:items-center">
+          <MobileSidebar></MobileSidebar>
+          {children}
+        </div>
+    </div>
   );
 }
